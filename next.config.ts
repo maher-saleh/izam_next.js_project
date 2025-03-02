2a768b7c06787c1587f6dcc65c53cf6ada390845
Maher Saleh <ehabelnahas@hotmail.com>
2025-03-02 22:37:40 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // Allow all websites to embed it
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' *", // Allow embedding on any site
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
